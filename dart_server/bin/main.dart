cda5b40421388402763ac30ab69cde125e8609e7
suragch <studymongolian@gmail.com>
2019-03-05 11:43:41 -0800

    initial commit

---
+++ dart_server/bin/main.dart
@@ -0,0 +1,17 @@
+import 'package:dart_server/dart_server.dart';
+
+Future main() async {
+  final app = Application<DartServerChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 3000; // default is 8888
+
+  //final count = Platform.numberOfProcessors ~/ 2;
+  //await app.start(numberOfInstances: count > 0 ? count : 1);
+  
+  // use the commented lines above for production code
+  // this is just to avoid bugs in our mock database
+  await app.start(numberOfInstances: 1);  
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
